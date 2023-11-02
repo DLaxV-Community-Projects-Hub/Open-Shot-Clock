028baabbcf41bd0235abc62a6ca20a7e534edbe0
Hendrik Ohrdes <devdrik@gmail.com>
2023-11-02 17:46:58 +0100

    refactored controller platformio.ini

--- code/controller/include/pinconfig.h
+++ code/controller/include/pinconfig.h
@@ -2,13 +2,9 @@
 #if defined(WIFI_LoRa_32_V2)
     #define RXD2 13
     #define TXD2 12
-
-    #define Vext 21
 #endif
 
 #if defined(WIFI_LoRa_32_V3)
     #define RXD2 6
     #define TXD2 5
-
-    #define Vext 7
 #endif
